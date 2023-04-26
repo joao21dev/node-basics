19455cc533457ad7b3318b5c33f5e9adb80e102c
joao21dev <joao.santos@in8.com.br>
2023-04-25 22:43:59 -0300

    req and res

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("🚀 ~ file: app.js:4 ~ http.createServer ~ req:", req);
+  res.write(JSON.stringify("Testeee"));
 });
 
 const port = 3000;
 
 server.listen(port, () => {
   console.log(`server listening at port ${port}`);
-  process.exit();
 });
