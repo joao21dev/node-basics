252a3e2827e15674229b8548c5339cb3aa3e8e28
joao21dev <joao21dev@gmail.com>
2023-05-12 09:27:14 -0300

    using modules export

--- app.js
+++ app.js
@@ -1,41 +1,8 @@
 const http = require("http");
-const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Minha primeira página</title></head>");
-    res.write(
-      "<body><form action= '/message' method='POST'><input type'text' name='message'><button type='submit'>Send</button</form></body>"
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-      console.log("🚀 ~ file: app.js:20 ~ req.on ~ chunk:", chunk);
-    });
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFileSync("message.txt", message);
-    });
-    //redirect status
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>Minha primeira página</title></head>");
-  res.write("<body><h1>Olá</h1></body>");
-  res.write("</html>");
-  res.end();
-});
+const routes = require("./routes");
+
+const server = http.createServer(routes);
 
 const port = 3000;
 
